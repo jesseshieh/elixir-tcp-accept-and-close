14b739b1f5af016eb7e8c2302dc5ef63e212f482
Jesse <jesse@gigalixir.com>
2018-12-06 12:19:36 -0800

    add debug logging.

--- lib/gen_tcp_accept_and_close/server.ex
+++ lib/gen_tcp_accept_and_close/server.ex
@@ -1,10 +1,12 @@
 defmodule GenTcpAcceptAndClose.Server do
   use GenServer
+  require Logger
 
   # from https://medium.com/blackode/quick-easy-tcp-genserver-with-elixir-and-erlang-10189b25e221
   def start_link(_) do
     ip = Application.get_env(:gen_tcp_accept_and_close, :ip, {0, 0, 0, 0})
     port = Application.get_env(:gen_tcp_accept_and_close, :port, 4000)
+    Logger.debug "[gen_tcp_accept_and_close] Starting #{__MODULE__} listening on #{to_string(:inet_parse.ntoa(ip))}:#{port}"
     GenServer.start_link(__MODULE__, [ip, port], [])
   end
 
@@ -18,6 +20,7 @@ defmodule GenTcpAcceptAndClose.Server do
   def handle_info(:timeout, {listen_socket}) do
     # accept and close right away ignoring whatever data is sent.
     {:ok, socket} = :gen_tcp.accept(listen_socket)
+    Logger.debug "[gen_tcp_accept_and_close] Accepting and closing connection"
     :ok = :gen_tcp.close(socket)
 
     # create an infinite loop
