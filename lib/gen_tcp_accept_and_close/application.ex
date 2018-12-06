72c182d379c05c73ca561919058338e3448cf832
Jesse <jesse@gigalixir.com>
2018-12-06 12:14:01 -0800

    forgot these.

---
+++ lib/gen_tcp_accept_and_close/application.ex
@@ -0,0 +1,8 @@
+defmodule GenTcpAcceptAndClose.Application do
+  use Application
+
+  def start(_type, _args) do
+    children = [GenTcpAcceptAndClose.Server]
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+end
